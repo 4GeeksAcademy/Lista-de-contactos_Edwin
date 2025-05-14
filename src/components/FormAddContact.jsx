581d5dde20c688761bc28a333822981022ed5732
Edwin González <130111056+edslogia@users.noreply.github.com>
2025-05-14 17:40:07 +0000

    componente de formulario creado visualmente

---
+++ src/components/FormAddContact.jsx
@@ -0,0 +1,50 @@
+export const FormAddContact = () => (
+  <div>
+    <div className="mb-3">
+      <label for="formGroupExampleInput" className="form-label">
+        Full Name
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter full name"
+      ></input>
+    </div>
+    <div className="mb-3">
+      <label for="formGroupExampleInput2" className="form-label">
+        Email
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter email"
+      >
+      </input>
+    </div>
+    <div className="mb-3">
+      <label for="formGroupExampleInput2" className="form-label">
+        Phone
+      </label>
+      <input
+        type="number"
+        className="form-control"
+        placeholder="Enter phone number"
+      >
+      </input>
+    </div>
+    <div className="mb-3">
+      <label for="formGroupExampleInput2" className="form-label">
+        Address
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter address"
+      >
+      </input>
+    </div>
+    <div className="mb-3">
+      <button type="button" class="btn btn-primary w-100">Save</button>
+    </div>
+  </div>
+);
---
+++ src/pages/AddContact.jsx
@@ -0,0 +1,21 @@
+// Import necessary components from react-router-dom and other parts of the application.
+import { Link } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";  // Custom hook for accessing the global state.
+import { FormAddContact } from "../components/FormAddContact"
+
+export const AddContact = () => {
+  // Access the global state and dispatch function using the useGlobalReducer hook.
+  const { store, dispatch } = useGlobalReducer()
+
+  return (
+    <div className="container">
+      <h1 className="text-center">Add new contact</h1>
+      <FormAddContact/>
+      <br />
+
+      <Link to="/">
+        <button className="btn btn-primary">Back home</button>
+      </Link>
+    </div>
+  );
+};
